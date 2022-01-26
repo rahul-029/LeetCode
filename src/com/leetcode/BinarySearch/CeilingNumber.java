99bd3d81f340d1580842bd655649a07a7b362e42
rahul-029 <rahul.29913@gmail.com>
2022-01-26 19:49:11 +0530

    Changes done to CeilingNumber program.

--- src/com/leetcode/BinarySearch/CeilingNumber.java
+++ src/com/leetcode/BinarySearch/CeilingNumber.java
@@ -6,7 +6,7 @@ public class CeilingNumber {
 
     public static void main(String[] args) {
         int arr[] = {2,3,5,9,14,16,18};
-        int target = 15;
+        int target = 18;
         int ans = findCeilingNumber(arr, target);
         System.out.println("Ceiling of the number: " + ans);
     }
@@ -14,24 +14,19 @@ public class CeilingNumber {
     private static int findCeilingNumber(int[] arr, int target) {
         int start = 0;
         int end = arr.length -1;
-        int mid = (start + end)/2;
 
-        if(arr[mid] == target){
-            return arr[mid];
-        }
-        else if(arr[mid] < target){
-            start = mid;
-        }
-        else{
-            end = mid;
-        }
 
-        for(int i=start; i <= end; i++){
-            if(arr[i] >= target){
-                return arr[i];
+        while(start < end) {
+            int mid = (start + end)/2;
+            if (arr[mid] == target) {
+                return arr[mid];
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid;
             }
         }
 
-        return 0;
+        return arr[start];
     }
 }

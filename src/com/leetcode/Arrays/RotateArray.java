6de2d8388d870acad40ffb1601974ef744e16e7c
rahul-029 <91196825+rahul-029@users.noreply.github.com>
2022-01-27 19:22:10 +0530

    RotateArray

---
+++ src/com/leetcode/Arrays/RotateArray.java
@@ -0,0 +1,31 @@
+package com.leetcode.Arrays;
+
+import java.util.Arrays;
+
+public class RotateArray {
+
+    public static void main(String[] args) {
+
+        int [] arr = {1, 2, 3, 4, 5, 6};
+        int n = 2;  // Rotate array "n" times. Ex- if n=1, O/P = [6, 1, 2, 3, 4, 5]
+                    // if n=3, O/P = [4, 5, 6, 1, 2, 3]
+
+        int [] ans = rotateArray(arr, n);
+        System.out.println("Array after rotation: " + Arrays.toString(arr));
+    }
+
+    private static int[] rotateArray(int[] arr, int n) {
+
+        //int count = 0;
+
+        for(int i=0; i < n; i++){
+            for(int j= arr.length-1; j > 0; j--){
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+            }
+        }
+
+        return arr;
+    }
+}

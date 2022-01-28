6a4fe56cc0dcdfdde3f417670b69dc03460ab819
rahul-029 <rahul.29913@gmail.com>
2022-01-29 01:59:00 +0530

    Bubble Sort

---
+++ src/com/leetcode/Sorting/BubbleSort.java
@@ -0,0 +1,34 @@
+package com.leetcode.Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        int [] arr = {1, 2, 3, 4, 5};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubbleSort(int[] arr) {
+
+        boolean swapped = false;
+        for(int i=0; i < arr.length; i++){
+            for(int j=1; j < arr.length - i; j++){  // 2nd loop runs till arr.length - i, because at every pass, max element will be at the end.
+
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if(!swapped){ // if list is already sorted, there will be no swaps, hence, swapped = false and will break out of th eloop
+                break;
+            }
+
+        }
+    }
+}

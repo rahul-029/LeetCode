a7eda05322ff7d2ec2a657a10567f333ce5672f9
rahul-029 <91196825+rahul-029@users.noreply.github.com>
2022-02-03 18:25:52 +0530

    return n and not 1

--- src/com/leetcode/RecursionBasics/Fibonacci.java
+++ src/com/leetcode/RecursionBasics/Fibonacci.java
@@ -12,7 +12,7 @@ public class Fibonacci {
     private static int fibo(int n) {
 
         if(n < 2){
-            return 1;
+            return n;
         }
 
         return fibo(n-1) + fibo(n-2);

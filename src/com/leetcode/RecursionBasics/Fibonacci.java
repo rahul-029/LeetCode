6921fe0ca12e8d1fe41e290e55188f713d7a2462
rahul-029 <rahul.29913@gmail.com>
2022-02-03 18:13:18 +0530

    Nth Fibonacci number.

---
+++ src/com/leetcode/RecursionBasics/Fibonacci.java
@@ -0,0 +1,20 @@
+package com.leetcode.RecursionBasics;
+
+// Find the nth fibonacci number,
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        int n = 5;
+        int ans = fibo(n);
+        System.out.println(ans);
+    }
+
+    private static int fibo(int n) {
+
+        if(n < 2){
+            return 1;
+        }
+
+        return fibo(n-1) + fibo(n-2);
+    }
+}

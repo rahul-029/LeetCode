c78f834b73ceb2975b9f7ab36c2a943b6d27ac8c
rahul-029 <rahul.29913@gmail.com>
2022-02-09 00:46:18 +0530

    Skipstring recursion program

---
+++ src/com/leetcode/RecursionString/SkipString.java
@@ -0,0 +1,57 @@
+package com.leetcode.RecursionString;
+
+public class SkipString {
+    public static void main(String[] args) {
+        String originalString = "bacchad";
+        String ans = "";
+        skip(ans, originalString);
+        System.out.println(skip2(originalString));
+        System.out.println(skipApple("baccapplehad"));
+    }
+
+    private static void skip(String ans, String originalString) {
+
+        if(originalString.isEmpty()){
+            System.out.println(ans);
+            return;
+        }
+
+        char ch = originalString.charAt(0);
+        if(ch == 'a'){
+            skip(ans, originalString.substring(1));
+        } else {
+            skip(ans + ch, originalString.substring(1));
+
+        }
+    }
+
+    private static String skip2(String originalString){
+
+        if(originalString.isEmpty()){
+            return " ";
+        }
+
+        char ch = originalString.charAt(0);
+        if(ch == 'a'){
+            return skip2(originalString.substring(1));
+        } else{
+            return ch + skip2(originalString.substring(1));
+        }
+    }
+
+    private static String skipApple(String originalString){
+
+        if(originalString.isEmpty()){
+            return "";
+        }
+
+        char ch = originalString.charAt(0);
+        if(originalString.startsWith("apple")){
+            return skipApple(originalString.substring(5));
+        } else {
+            return ch + skipApple(originalString.substring(1));
+        }
+    }
+
+
+}

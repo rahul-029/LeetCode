e532dc94ae4fcf46b268e6f1145522e34a3a8c21
rahul-029 <rahul.29913@gmail.com>
2022-02-24 23:02:01 +0530

    Linked List Node class

---
+++ src/com/leetcode/LinkedLists/SinglyLL.java
@@ -0,0 +1,50 @@
+package com.leetcode.LinkedLists;
+
+public class SinglyLL {
+
+    private Node head;
+    private Node tail;
+    private int size = 0;
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void insertAtFirst(int value){
+
+        Node node = new Node(value);
+        if(head == null){
+            head = node;
+            tail = head;
+        } else {
+            node.next = head;
+            head = node;
+        }
+
+        size++;
+    }
+
+    public void insertAtLast(int value){
+
+        Node node = new Node(value);
+        tail.next = node;
+        tail = node;
+
+        size++;
+    }
+
+    public void display(){
+
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.value + " --> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+}

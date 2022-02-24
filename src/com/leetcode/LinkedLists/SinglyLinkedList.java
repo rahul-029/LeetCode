40b4bcdb0eaf29a45db407cbcec96ad68f6eb51b
rahul-029 <rahul.29913@gmail.com>
2022-02-24 23:02:29 +0530

    Linked List SLL Driver class.

---
+++ src/com/leetcode/LinkedLists/SinglyLinkedList.java
@@ -0,0 +1,17 @@
+package com.leetcode.LinkedLists;
+
+public class SinglyLinkedList {
+
+    public static void main(String[] args) {
+
+        SinglyLL sll = new SinglyLL();
+        sll.insertAtFirst(4);
+        sll.insertAtFirst(3);
+        sll.insertAtFirst(2);
+        sll.insertAtFirst(1);
+        sll.insertAtLast(5);
+
+        sll.display();
+
+    }
+}

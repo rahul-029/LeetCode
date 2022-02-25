9fa5650cf07934b5632749eb9c5b9622c6e0ddc7
rahul-029 <rahul.29913@gmail.com>
2022-02-26 00:22:05 +0530

    Driver code

--- src/com/leetcode/LinkedLists/DoublyLinkedList.java
+++ src/com/leetcode/LinkedLists/DoublyLinkedList.java
@@ -5,10 +5,18 @@ public class DoublyLinkedList {
     public static void main(String[] args) {
 
         DoublyLL dll = new DoublyLL();
-        dll.insertAtFirst(4);
+        /*dll.insertAtFirst(4);
         dll.insertAtFirst(3);
         dll.insertAtFirst(2);
         dll.insertAtFirst(1);
+        dll.insertAtLast(5);*/
+
+        dll.insertAtLast(1);
+        dll.insertAtLast(2);
+        dll.insertAtLast(3);
+        dll.insertAtLast(4);
+        dll.insertAtLast(5);
+
 
         dll.display();
 

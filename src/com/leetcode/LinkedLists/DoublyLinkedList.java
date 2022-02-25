8b8f9bff80a46349419e8c2b4e549869d755f353
rahul-029 <rahul.29913@gmail.com>
2022-02-26 00:10:45 +0530

    DoublyLinkedList driver class

---
+++ src/com/leetcode/LinkedLists/DoublyLinkedList.java
@@ -0,0 +1,16 @@
+package com.leetcode.LinkedLists;
+
+public class DoublyLinkedList {
+
+    public static void main(String[] args) {
+
+        DoublyLL dll = new DoublyLL();
+        dll.insertAtFirst(4);
+        dll.insertAtFirst(3);
+        dll.insertAtFirst(2);
+        dll.insertAtFirst(1);
+
+        dll.display();
+
+    }
+}

782ec622639f03e0b484b96347ad818c20cb380c
rahul-029 <rahul.29913@gmail.com>
2022-02-26 19:12:33 +0530

    LL exercise

---
+++ src/com/leetcode/LinkedListQuestions/SinglyLinkedListExercise.java
@@ -0,0 +1,16 @@
+package com.leetcode.LinkedListQuestions;
+
+public class SinglyLinkedListExercise {
+
+    public static void main(String[] args) {
+        SinglyLL sll = new SinglyLL();
+        sll.insertAtLast(1);
+        sll.insertAtLast(1);
+        sll.insertAtLast(1);
+        sll.insertAtLast(2);
+        sll.insertAtLast(4);
+        sll.insertAtLast(4);
+
+        sll.display();
+    }
+}

222201799df11e064343dad61d51956fd3f2d302
rahul-029 <rahul.29913@gmail.com>
2022-02-26 11:29:03 +0530

    CircularLinkedList driver class.

---
+++ src/com/leetcode/LinkedLists/CircularLinkedList.java
@@ -0,0 +1,16 @@
+package com.leetcode.LinkedLists;
+
+public class CircularLinkedList {
+
+    public static void main(String[] args) {
+
+        CircularLL cll = new CircularLL();
+        cll.insert(1);
+        cll.insert(2);
+        cll.insert(3);
+        cll.insert(4);
+
+        cll.display();
+
+    }
+}

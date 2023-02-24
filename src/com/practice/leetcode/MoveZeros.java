23e73df5b354a28f835aa5dc5b06f7b66f13dd7d
rahul-029 <rahul.29913@gmail.com>
2023-02-24 22:32:02 +0530

    Move all zeros to the right in place and elements in order.

---
+++ src/com/practice/leetcode/MoveZeros.java
@@ -0,0 +1,28 @@
+package com.practice.leetcode;
+
+import java.util.Arrays;
+
+public class MoveZeros {
+
+    public static void main(String[] args) {
+
+        int [] arr = {0,1,0,3,12};
+        moveZeroes(arr);
+    }
+
+    public static void moveZeroes(int[] nums) {
+
+        int snowBallSize = 0;
+        for(int i=0; i < nums.length; i++){
+            if(nums[i] == 0){
+                snowBallSize++;
+            } else if(snowBallSize > 0){
+                int temp = nums[i];
+                nums[i] = 0;
+                nums[i-snowBallSize] = temp;
+            }
+        }
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
